309d4fe3fb1fd9d4382e940643d85661edc1b533
touza <touzaelhassan@gmail.com>
2022-04-08 17:47:09 +0000

    Adding categories to the sidebar section

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,3 +1,5 @@
+<?php $categories = get_categories(); ?>
+
 <div class="sidebar__content">
   <div class="sidebar__search">
     <form action="./search.php" method="POST" class="search__form">
@@ -6,5 +8,14 @@
     </form>
   </div>
   <div class="sidebar__login">LOGIN</div>
-  <div class="sidebar__categories">CATEGORIES</div>
+  <div class="sidebar__categories">
+    <ul class="categories__list">
+      <?php foreach ($categories as $category) : ?>
+        <li class="categories__item">
+          <a href="#" class="categories__link"><?php echo $category["category_title"]; ?></a>
+        </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
+  <div class="sidebar__recent-posts">RECENT POSTS</div>
 </div>
