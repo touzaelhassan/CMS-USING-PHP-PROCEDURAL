7e4d0290192943bb6b28af5354324b235a7a4f43
touza <touzaelhassan@gmail.com>
2022-04-08 18:38:38 +0000

    Creating reuseable code in the admin dashboard

---
+++ admin/includes/header.php
@@ -0,0 +1,8 @@
+<header class="header">
+  <div class="header__content">
+    <div class="logo"><a class="logo__link" href="index.php">CMS</a></div>
+    <nav class="navigation">
+      NAVIGATION
+    </nav>
+  </div>
+</header>
---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php include './includes/head.php' ?>
+
+
+<?php include './includes/header.php' ?>
+
+<div class="page">
+  <aside class="sidebar">
+  </aside>
+  <div class="dashboard">
+  </div>
+</div>
+
+<?php include './includes/footer.php' ?>
+
+<?php include './includes/foot.php' ?>
