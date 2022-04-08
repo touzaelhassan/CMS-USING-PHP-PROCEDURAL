3ffe6e221414e3455e775d85745c2604f8e7420e
touza <touzaelhassan@gmail.com>
2022-04-08 01:28:32 +0000

    Creating home page structure and making project files reusable

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include './includes/head.php' ?>
+
+<?php include './includes/header.php' ?>
+<div class="page">
+  <div class="posts"></div>
+  <aside class="sidebar"></aside>
+</div>
+<?php include './includes/footer.php' ?>
+
+<?php include './includes/foot.php' ?>
