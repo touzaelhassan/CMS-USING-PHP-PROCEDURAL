948f48e8107f4f27ef035c91029376e6056fb6d9
touza <touzaelhassan@gmail.com>
2022-04-08 12:31:16 +0000

    Inserting data into posts table and displaying in the home page

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -11,3 +11,17 @@ function get_categories()
 
 ?>
 <!-- End Categories Functions -->
+
+<!-- Start Posts Functions -->
+<?php
+
+function get_posts()
+{
+  global $connection;
+  $sql = "SELECT * FROM posts";
+  $query = mysqli_query($connection, $sql);
+  return mysqli_fetch_all($query, MYSQLI_ASSOC);
+}
+
+?>
+<!-- End Posts Functions -->
--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php include './includes/head.php' ?>
 
+<?php $posts = get_posts(); ?>
+
 <?php include './includes/header.php' ?>
 <div class="page">
-  <div class="posts"></div>
+  <div class="posts">
+    <div class="posts__content">
+      <?php foreach ($posts as $post) : ?>
+        <div class="post">
+          <h2 class="post__title"><?php echo $post["post_title"]; ?></h2>
+          <p class="post__author"><?php echo $post["post_author"]; ?></p>
+          <p class="post__date"><?php echo $post["post_date"]; ?></p>
+          <div class="post__image">
+            <img src="./images/<?php echo $post["post_id"]; ?>.jpg">
+          </div>
+          <p class="post__content"><?php echo $post["post_content"]; ?></p>
+          <a href="#" class="post__link">READ MORE</a>
+        </div>
+      <?php endforeach ?>
+    </div>
+    <div class="posts__pagination">
+      PAGINATION
+    </div>
+  </div>
   <aside class="sidebar"></aside>
 </div>
 <?php include './includes/footer.php' ?>
