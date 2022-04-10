353f8eb2acfb250842d02696cf30b38b61f5e6f3
touza <touzaelhassan@gmail.com>
2022-04-10 17:54:48 +0000

    displaying post based on status draft or publish

--- index.php
+++ index.php
@@ -8,16 +8,18 @@
   <div class="posts">
     <div class="posts__content">
       <?php foreach ($posts as $post) : ?>
-        <div class="post">
-          <h2 class="post__title"><a href="post.php?post_id=<?php echo $post["post_id"] ?>"><?php echo $post["post_title"]; ?></a></h2>
-          <p class="post__author"><?php echo $post["post_author"]; ?></p>
-          <p class="post__date"><?php echo $post["post_date"]; ?></p>
-          <div class="post__image">
-            <img src="./images/<?php echo $post["post_image"]; ?>">
+        <?php if ($post["post_status"] == "publish") : ?>
+          <div class="post">
+            <h2 class="post__title"><a href="post.php?post_id=<?php echo $post["post_id"] ?>"><?php echo $post["post_title"]; ?></a></h2>
+            <p class="post__author"><?php echo $post["post_author"]; ?></p>
+            <p class="post__date"><?php echo $post["post_date"]; ?></p>
+            <div class="post__image">
+              <img src="./images/<?php echo $post["post_image"]; ?>">
+            </div>
+            <p class="post__content"><?php echo substr($post["post_content"], 0, 300); ?></p>
+            <a href="#" class="post__link">READ MORE</a>
           </div>
-          <p class="post__content"><?php echo substr($post["post_content"], 0, 300); ?></p>
-          <a href="#" class="post__link">READ MORE</a>
-        </div>
+        <?php endif; ?>
       <?php endforeach ?>
     </div>
     <div class="posts__pagination">
