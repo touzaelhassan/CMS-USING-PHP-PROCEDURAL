a6a1f28f6289d9a8d17ee7e213cc9eda237711cc
touza <touzaelhassan@gmail.com>
2022-04-11 17:07:50 +0000

    create select user query

--- includes/login.php
+++ includes/login.php
@@ -6,6 +6,13 @@
 if (isset($_POST['login'])) {
   $user_name = $_POST['user_name'];
   $user_password = $_POST['user_password'];
+
+  $user_name = mysqli_real_escape_string($connection, $user_name);
+  $user_password = mysqli_real_escape_string($connection, $user_password);
+
+  $sql = "SELECT * FROM users WHERE user_name = '$user_name' AND user_password = '$user_password'";
+  $query = mysqli_query($connection, $sql);
+  $user = mysqli_fetch_assoc($query);
 }
 
 ?>
