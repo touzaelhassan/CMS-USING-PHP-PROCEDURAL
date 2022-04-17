e903902e0bad832c5021c5af222a9334727d154f
touza <touzaelhassan@gmail.com>
2022-04-17 20:40:23 +0000

    add admin name dynamically

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,5 +3,8 @@
     <div class="logo me-auto"><a class="logo__link" href="../index.php">CMS</a></div>
     <div class="logout"><a class="logout__link" href="./includes/logout.php">LOGOUT</a></div>
     <div class="homepage"><a class="homepage__link" href="../index.php">HOMEPAGE</a></div>
+    <?php if (isset($_SESSION["user_name"])) : ?>
+      <div class="admin-name"><i class="fas fa-user"></i><span class="text-uppercase"><?php echo $_SESSION["user_name"]; ?></span></div>
+    <?php endif ?>
   </div>
 </header>
--- includes/header.php
+++ includes/header.php
@@ -21,5 +21,8 @@
       <div class="logout"><a class="logout__link" href="./admin/includes/logout.php">LOGOUT</a></div>
     <?php endif ?>
     <div class="admin"><a href="./admin/index.php">DASHBOARD</a></div>
+    <?php if (isset($_SESSION["user_name"])) : ?>
+      <div class="admin-name"><i class="fas fa-user"></i><span class="text-uppercase"><?php echo $_SESSION["user_name"]; ?></span></div>
+    <?php endif ?>
   </div>
 </header>
